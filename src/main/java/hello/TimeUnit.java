8fd4f211aaeaaa474d3160da1b2fabeff2cf4e3a
Glenn Renfro <grenfro@pivotal.io>
2015-09-10 10:26:26 -0400

    Code cleanup

--- src/main/java/hello/TimeUnit.java
+++ src/main/java/hello/TimeUnit.java
@@ -1,11 +1,21 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package hello;
 
-/**
- * Helps with conversion between units of time. Similar in spirit to {@link java.util.concurrent.TimeUnit}, but
- * the conversion method uses double and does not truncate.
- *
- * @author Eric Bottard
- */
 public enum TimeUnit {
 	ns(1),
 	ms(1000),
